a2867b1dd27667bb3fa6be76eaf2788ee9f3eee8
hcsproyec <hcsproyec@gmail.com>
2025-05-12 15:19:52 -0600

    Add files via upload

---
+++ growth_tracking.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-05-2025 a las 23:17:37
+-- Versión del servidor: 8.0.41
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `growth_tracking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `Id` int NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Contacto` varchar(255) DEFAULT NULL,
+  `Mail` varchar(255) DEFAULT NULL,
+  `Celular` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `matriz`
+--
+
+CREATE TABLE `matriz` (
+  `Id` int NOT NULL,
+  `Fecha_Inicio` date NOT NULL,
+  `Fecha_Finalizacion` date DEFAULT NULL,
+  `Cliente_Prospecto` varchar(255) NOT NULL,
+  `Nombre_contacto` varchar(255) DEFAULT NULL,
+  `Mail` varchar(255) DEFAULT NULL,
+  `Celular` varchar(20) DEFAULT NULL,
+  `Lead_Agency` varchar(255) DEFAULT NULL,
+  `Proyecto_Servicio` varchar(255) DEFAULT NULL,
+  `Annual_Value` decimal(10,2) DEFAULT NULL,
+  `Type` enum('NB','Cross') DEFAULT NULL,
+  `Probabilidad` decimal(3,2) DEFAULT NULL,
+  `Owner` varchar(255) DEFAULT NULL,
+  `Notes` text,
+  `Status` enum('Nuevo','En Progreso','Cerrado') DEFAULT NULL,
+  `Prioridad` enum('Alta','Media','Baja') DEFAULT NULL,
+  `Referral` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `Id` int NOT NULL,
+  `Nombre` varchar(255) NOT NULL,
+  `Contraseña` varchar(255) NOT NULL,
+  `Tipo` enum('Administrador','Usuario') DEFAULT 'Usuario'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `matriz`
+--
+ALTER TABLE `matriz`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `Id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `matriz`
+--
+ALTER TABLE `matriz`
+  MODIFY `Id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `Id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
